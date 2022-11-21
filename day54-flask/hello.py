614a80d9fe544b81965fde0aa10a23e4e9c884a9
findamak <findamak@gmail.com>
2022-11-21 11:27:23 +1100

    day 54 complete

---
+++ day54-flask/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+# This is known as a python decorator function
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# This allows us to run this application without having to set a windows environment variable using
+# '$env:FLASK_APP = "hello.py"' and then typing flask run in the command line. Running this file will start the server.
+if __name__ == "__main__":
+    app.run()
