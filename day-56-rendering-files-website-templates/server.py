3c60848368ea285cd447c002b64e8044e9c2af6c
findamak <findamak@gmail.com>
2023-01-10 20:15:18 +1100

    day 56 complete

---
+++ day-56-rendering-files-website-templates/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+# This allows us to run this application without having to set a windows environment variable using
+# '$env:FLASK_APP = "hello.py"' and then typing flask run in the command line. Running this file will start the server.
+if __name__ == "__main__":
+    # Turn on debug which also reloads the webserver whenever changes are made to this file.
+    app.run(debug=True)
